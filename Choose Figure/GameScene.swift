71096594f20306e85a9731160d899ce37510d2c3
ISosnovik <sosnovikivan@gmail.com>
2016-02-10 16:05:36 +0300

    added scene

--- Choose Figure/GameScene.swift
+++ Choose Figure/GameScene.swift
@@ -8,38 +8,56 @@
 
 import SpriteKit
 
+let bestScoreKey = "BestScore"
+
 class GameScene: SKScene {
-    override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
-        
-        self.addChild(myLabel)
-    }
     
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
+    // Score settings
+    private var bestScore: Int? {
+        get {
+            return NSUserDefaults.standardUserDefaults().integerForKey(bestScoreKey) ?? 0
+        }
         
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
+        set {
+            NSUserDefaults.standardUserDefaults().setInteger(newValue!, forKey: bestScoreKey)
+            NSUserDefaults.standardUserDefaults().synchronize()
         }
     }
-   
-    override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
+    
+    var currentScore: Int? {
+        
+        get {
+            return self.currentScore ?? 0
+        }
+
+        set {
+            self.currentScore = newValue
+            if currentScore > bestScore {
+                self.bestScore = currentScore
+            }
+        }
     }
+    
+    // Main lifecycle setups
+    override func didMoveToView(view: SKView) {
+        
+        
+    }
+    
 }
+
+
+extension GameScene {
+    
+    func newLevel() {
+        
+    }
+        
+}
+
+
+
+
+
+
+
